f5001cadd35036427210b2b002dd2166e68c006b
ceniscool <143891100+ceniscool@users.noreply.github.com>
2025-07-27 20:51:05 -0400

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "SHOW_NOTIFICATION") {
     chrome.notifications.create({
       type: "basic",
-      iconUrl: "rsz_maxresdefault.jpg", // You can change or remove this if no icon
+      iconUrl: "rsz_maxresdefault.png", // You can change or remove this if no icon
       title: message.title,
       message: message.message
     });
